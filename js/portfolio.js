e008fcf3b7df187c2d2e01a20c576adca1bcee09
Julia O'Flynn O'Brien <me@jofob.com>
2015-08-19 09:56:42 +0100

    Adding structure

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,12 @@
-window.onload = setup;
+document.body.onload = setup;
 
 /* -GLOBAL VARIABLES- */
 var header;
+var pieces;
 
 function setup(){
 	header = document.getElementById("frontHead");
+	buildPage();
 	window.addEventListener('scroll', function(event){
 			var distanceY = window.pageYOffset || document.documentElement.scrollTop;
 			if (distanceY >= 100) {
@@ -21,4 +23,18 @@ function shrinkHeader(){
 
 function reset(){
 	header.classList.remove("small");
+}
+
+function buildPage(){
+	pieces = document.getElementById("pieces")
+	var idx = 0;
+	while (idx < portPieces.length){
+		var currPiece = portPieces[idx];
+		var div = document.createElement('div');
+		pieces.appendChild(div);
+		div.ID = "p"+ toString(idx);
+		div.className= "portPiece";
+		div.innerHTML = currPiece.desc;
+		idx++; 
+	}
 }
---
+++ js/portfolioPieces.js
@@ -0,0 +1,28 @@
+//Pieces included in portfolio
+var portPieces = [
+
+		{
+
+			"title":  "Sokoban",
+			"desc":   "Puzzle game built in javaScript",
+			"alt":    "Sokoban",
+			"cat":    "prog",
+			"href":   "/sokoban.html",
+			"imgs":  ["dest,dest"]},
+		{
+
+			"title":  "MemoryBox",
+			"desc":   "Assistive, comfort technology for alzheimer's patients",
+			"alt":    "text",
+			"cat":    "group",
+			"href":   "link",
+			"imgs":  ["dest,dest"]},
+		{
+
+			"title":  "piece",
+			"desc":   "words",
+			"alt":    "text",
+			"cat":    "group",
+			"href":   "link",
+			"imgs":  ["dest,dest"]}
+]
