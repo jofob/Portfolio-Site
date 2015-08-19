637b3add39b0e293fef778ab27497aa520cbc8ab
Julia O'Flynn O'Brien <oflynnoj@tcd.ie>
2015-08-19 17:12:22 +0100

    styling changes

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,6 +8,23 @@ var nav;
 var tag;
 var wasBig = true;
 
+$(function() {
+		  $('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			  if (target.length) {
+				$('html,body').animate({
+				  scrollTop: target.offset().top
+				}, 750, 'easeOutExpo');
+				return false;
+			  }
+			}
+		  });
+		});
+
+		
+		
 function setup(){
 	header = document.getElementById("frontHead");
 	logo = document.getElementById("logo");
